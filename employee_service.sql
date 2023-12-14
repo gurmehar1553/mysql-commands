90ad4a1447cf7ce6ab6c572bb85bca544e6db315
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-14 14:29:12 +0530

    Added er diagram

--- employee_service.sql
+++ employee_service.sql
@@ -177,3 +177,94 @@ mysql> SELECT MIN(salary) from employee_payroll where gender='F' group by gender
 |     1000000 |
 +-------------+
 1 row in set (0.07 sec)
+
+/** -------- UC8 -----------**/
+
+mysql> ALTER TABLE employee_payroll
+    -> ADD COLUMN phone VARCHAR(20),
+    -> ADD COLUMN address VARCHAR(100) DEFAULT '#12',
+    -> ADD COLUMN department VARCHAR(20) NOT NULL;
+Query OK, 0 rows affected (8.40 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> SELECT * FROM employee_payroll;
++----+-------+--------+---------+------------+-------+---------+------------+
+| id | name  | gender | salary  | start      | phone | address | department |
++----+-------+--------+---------+------------+-------+---------+------------+
+|  1 | Bill  | F      | 1000000 | 2021-07-09 | NULL  | #12     |            |
+|  2 | Mark  | M      | 1200000 | 2021-07-09 | NULL  | #12     |            |
+|  3 | Betty | F      | 2200000 | 2021-07-02 | NULL  | #12     |            |
+|  4 | John  | M      | 5000000 | 2021-04-06 | NULL  | #12     |            |
++----+-------+--------+---------+------------+-------+---------+------------+
+4 rows in set (0.00 sec)
+
+/** -------- UC9 -----------**/
+
+mysql> ALTER TABLE employee_payroll RENAME COLUMN salary TO basic_pay;
+Query OK, 0 rows affected (0.22 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> ALTER TABLE employee_payroll ADD deductions Double NOT NULL AFTER basic_pay;
+Query OK, 0 rows affected (0.53 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> ALTER TABLE employee_payroll ADD taxable_pay Double NOT NULL AFTER deductions;
+Query OK, 0 rows affected (5.20 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> ALTER TABLE employee_payroll ADD tax Double NOT NULL AFTER taxable_pay;
+Query OK, 0 rows affected (0.30 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> ALTER TABLE employee_payroll ADD net_pay Double NOT NULL AFTER tax;
+Query OK, 0 rows affected (6.04 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> select * from employee_payroll;
++----+-------+--------+-----------+------------+-------------+-----+---------+------------+-------+---------+------------+
+| id | name  | gender | basic_pay | deductions | taxable_pay | tax | net_pay | start      | phone | address | department |
++----+-------+--------+-----------+------------+-------------+-----+---------+------------+-------+---------+------------+
+|  1 | Bill  | F      |   1000000 |          0 |           0 |   0 |       0 | 2021-07-09 | NULL  | #12     |            |
+|  2 | Mark  | M      |   1200000 |          0 |           0 |   0 |       0 | 2021-07-09 | NULL  | #12     |            |
+|  3 | Betty | F      |   2200000 |          0 |           0 |   0 |       0 | 2021-07-02 | NULL  | #12     |            |
+|  4 | John  | M      |   5000000 |          0 |           0 |   0 |       0 | 2021-04-06 | NULL  | #12     |            |
++----+-------+--------+-----------+------------+-------------+-----+---------+------------+-------+---------+------------+
+4 rows in set (0.00 sec)
+
+/** -------- UC10 -----------**/
+
+
+mysql> INSERT INTO employee_payroll
+    -> (name, department, gender, basic_pay, deductions, taxable_pay, tax, net_pay, start) VALUES
+    -> ('Terisa', 'Marketting', 'F', 3000000.00, 1000000.00, 2000000.00, 500000.00, 1500000.00, '2018-01-03' );
+Query OK, 1 row affected (0.12 sec)
+
+mysql> select * from employee_payroll;
++----+--------+--------+-----------+------------+-------------+--------+---------+------------+-------+---------+------------+
+| id | name   | gender | basic_pay | deductions | taxable_pay | tax    | net_pay | start      | phone | address | department |
++----+--------+--------+-----------+------------+-------------+--------+---------+------------+-------+---------+------------+
+|  1 | Bill   | F      |   1000000 |          0 |           0 |      0 |       0 | 2021-07-09 | NULL  | #12     |            |
+|  2 | Mark   | M      |   1200000 |          0 |           0 |      0 |       0 | 2021-07-09 | NULL  | #12     |            |
+|  3 | Betty  | F      |   2200000 |          0 |           0 |      0 |       0 | 2021-07-02 | NULL  | #12     |            |
+|  4 | John   | M      |   5000000 |          0 |           0 |      0 |       0 | 2021-04-06 | NULL  | #12     |            |
+|  5 | Terisa | F      |   3000000 |    1000000 |     2000000 | 500000 | 1500000 | 2018-01-03 | NULL  | #12     | Marketting |
++----+--------+--------+-----------+------------+-------------+--------+---------+------------+-------+---------+------------+
+5 rows in set (0.00 sec)
+
+mysql> INSERT INTO employee_payroll
+    -> (name,gender,basic_pay,deductions,taxable_pay,tax,net_pay,department) VALUES
+    -> ('Terisa','F',3000000,0,0,0,0,'Sales');
+Query OK, 1 row affected (0.12 sec)
+
+mysql> select * from employee_payroll;
++----+--------+--------+-----------+------------+-------------+--------+---------+------------+-------+---------+------------+
+| id | name   | gender | basic_pay | deductions | taxable_pay | tax    | net_pay | start      | phone | address | department |
++----+--------+--------+-----------+------------+-------------+--------+---------+------------+-------+---------+------------+
+|  1 | Bill   | F      |   1000000 |          0 |           0 |      0 |       0 | 2021-07-09 | NULL  | #12     |            |
+|  2 | Mark   | M      |   1200000 |          0 |           0 |      0 |       0 | 2021-07-09 | NULL  | #12     |            |
+|  3 | Betty  | F      |   2200000 |          0 |           0 |      0 |       0 | 2021-07-02 | NULL  | #12     |            |
+|  4 | John   | M      |   5000000 |          0 |           0 |      0 |       0 | 2021-04-06 | NULL  | #12     |            |
+|  5 | Terisa | F      |   3000000 |    1000000 |     2000000 | 500000 | 1500000 | 2018-01-03 | NULL  | #12     | Marketting |
+|  6 | Terisa | F      |   3000000 |          0 |           0 |      0 |       0 | NULL       | NULL  | #12     | Sales      |
++----+--------+--------+-----------+------------+-------------+--------+---------+------------+-------+---------+------------+
+6 rows in set (0.00 sec)
